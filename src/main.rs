46d3dddfaf52cd8e3ce850e630d3bdc5bca8ed28
yhanyi <yeohhanyi0916@gmail.com>
2024-08-04 21:58:16 +0800

    Bugfix main call function

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@ mod web_interface;
 
 use crawler::Crawler;
 use search::SearchEngine;
-use std::sync::Arc;
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let seed_urls = vec![
+        "https://www.rust-lang.org".to_string(),
+        "https://doc.rust-lang.org".to_string()
+    ];
+    let crawler = Crawler::new(seed_urls, 2);
+    crawler.crawl();
+    let mut search_engine = SearchEngine::new();
+    for url in &["https://www.rust-lang.org", "https://doc.rust-lang.org"] {
+        search_engine.index_page(url)?;
+    }
+    web_interface::launch(search_engine)?;
+
+    Ok(())
 }
