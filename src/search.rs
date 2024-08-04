eb598b2475719ab42d9705e592263ff19723cc70
yhanyi <yeohhanyi0916@gmail.com>
2024-08-04 21:12:58 +0800

    Updated page search

--- src/search.rs
+++ src/search.rs
@@ -12,4 +12,22 @@ impl SearchEngine {
             index: HashMap::new(),
         }
     }
+
+    pub fn search(&self, query: &str) -> Vec<String> {
+        let query_words: Vec<String> = query.split_whitespace().map(str::to_lowercase).collect();
+        let mut results: HashMap<String, usize> = HashMap::new();
+        for word in query_words {
+            if let Some(urls) = self.index.get(&word) {
+                for url in urls {
+                    *results.entry(url.to_string()).or_default() += 1;
+                }
+            }
+        }
+        let mut sorted_results: Vec<_> = results.into_iter().collect();
+        sorted_results.sort_by(|a, b| b.1.cmp(&a.1));
+        sorted_results
+            .into_iter()
+            .map(|(url, _)| url)
+            .collect()
+    }
 }
